acb9a55a0a4bdf7f8eb78c5b2fe9346a71843236
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-19 17:47:20 +0000

    Done.

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var express = require('express')
 var app = express()
 
 // Build Your Route Here
+app.get('/', function(req, resp){
+  console.log(req)
+  console.log(resp)
 
+  resp.send("Hello, World!")
+})
 
 module.exports = app
